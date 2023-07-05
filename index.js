2640ccf51f86d1d183efaa242e43e6992cb4f56d
Gaspar Sio <sio.gaspar@gmail.com>
2023-07-05 14:37:09 +0200

    Changes on visual elements on hover property

--- index.js
+++ index.js
@@ -1,44 +1,15 @@
-const container = document.getElementById('container');
+// const imgOxy = document.querySelector(".img-oxy");
+// const imgMypho = document.querySelector(".img-mypho");
 
-container.addEventListener('wheel', (event) => {
-  event.preventDefault();
-  const deltaY = event.deltaY;
-  const scrollAmount = Math.abs(deltaY) > 100 ? 0.3 : 0.1; // Ajusta la velocidad de desplazamiento aquí
-  container.scrollTo({
-    top: container.scrollTop + deltaY * scrollAmount,
-    behavior: 'smooth'
-  });
-});
+// function animate() {
+//   imgOxy.addEventListener("mouseleave", function() {
+//     imgOxy.classList.add('magictime', 'openUpLeft');
+//   });
 
-
-
-// Obtener los elementos de los enlaces del menú
-const navLinks = document.querySelectorAll('nav ul li a');
-
-// Obtener las secciones
-const sections = document.querySelectorAll('section');
-
-// Función para cambiar el color del enlace activo
-function changeActiveLink() {
-  // Obtener la posición actual de desplazamiento
-  const scrollPosition = window.pageYOffset;
-
-  // Recorrer todas las secciones
-  sections.forEach((section, index) => {
-    // Obtener la posición superior e inferior de la sección
-    const sectionTop = section.offsetTop;
-    const sectionBottom = sectionTop + section.offsetHeight;
-
-    // Verificar si la posición actual de desplazamiento está dentro de la sección
-    if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-      // Agregar la clase activa al enlace correspondiente
-      navLinks.forEach((link) => {
-        link.classList.remove('active');
-      });
-      navLinks[index].classList.add('active');
-    }
-  });
-}
-
-// Evento de desplazamiento
-window.addEventListener('scroll', changeActiveLink);
+//   // Agregar evento de hover a la imagen de myPhotos
+//   imgMypho.addEventListener("mouseover", function() {
+//     imgMypho.classList.add('magictime', 'vanishIn');
+//   });
+//   console.log('asd');
+// }
+// animate();
