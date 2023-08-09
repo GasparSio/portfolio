ab073d5433ba1a3447f47aaa9d3a6f9957a22a6f
Gaspar Sio <sio.gaspar@gmail.com>
2023-08-09 18:07:58 +0200

    changes on sliderin section 3

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-// const imgOxy = document.querySelector(".img-oxy");
-// const imgMypho = document.querySelector(".img-mypho");
-
-// function animate() {
-//   imgOxy.addEventListener("mouseleave", function() {
-//     imgOxy.classList.add('magictime', 'openUpLeft');
-//   });
-
-//   // Agregar evento de hover a la imagen de myPhotos
-//   imgMypho.addEventListener("mouseover", function() {
-//     imgMypho.classList.add('magictime', 'vanishIn');
-//   });
-//   console.log('asd');
-// }
-// animate();
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
