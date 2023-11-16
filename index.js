722206660c4e6504b39f5c59d8bdbe5eba07e057
Gaspar Sio <sio.gaspar@gmail.com>
2023-11-16 23:36:19 +0100

    changes on swiper

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+// Arrow animation when scroll
+// const arrowContainer = document.querySelector('.arrow');
+// const container = document.getElementById('container');
+
+// let isScrolling = false;
+
+// container.addEventListener('wheel', function () {
+//   const containerScrollTop = container.scrollTop || document.documentElement.scrollTop;
+
+//   if (containerScrollTop > 0) {
+//     arrowContainer.style.display = 'block';
+
+//     // Si no se está ya ejecutando el temporizador, configúralo
+//     if (!isScrolling) {
+//       isScrolling = true;
+
+//       // Oculta la flecha después de 1 segundo
+//       setTimeout(function () {
+//         arrowContainer.style.display = 'none';
+//         isScrolling = false;
+//       }, 1000);
+//     }
+//   }
+// });
+
+
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
@@ -19,3 +46,14 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+
+
+
+
+
+
+
+
+
+
+
